6f92786c2ec81f6ddb08b2d2ea374f710b09eea8
Juan Pablo <eljuan1820@gmail.com>
2021-04-29 14:53:12 -0500

    prueba

---
+++ src/parcial_2/animales.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial_2;
+
+/**
+ *
+ * @author juanp
+ */
+public abstract class animales implements ComporAnimal{
+   
+   private String Tamaño;
+   private String color;
+}
